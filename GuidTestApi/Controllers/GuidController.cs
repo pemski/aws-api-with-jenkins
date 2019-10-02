2f6d858f8e0e148cd5e40e5f0a04cdb1e421d6aa
Przemyslaw S <przmk01@gmail.com>
2019-10-02 21:28:27 +0200

    Nonsense API project added

---
+++ GuidTestApi/Controllers/GuidController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GuidTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GuidController : ControllerBase
+    {
+        private readonly ILogger<GuidController> _logger;
+        private readonly static Guid Guid = Guid.NewGuid();
+
+        public GuidController(ILogger<GuidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return Guid.ToString();
+        }
+    }
+}
