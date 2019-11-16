409293152e5dc54ac24ceae51f5ee9bbf4016513
Przemyslaw S <przmk01@gmail.com>
2019-11-16 23:22:26 +0100

    Lambda function added

---
+++ GuidGetter/GuidGetter.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Lambda.Core;
+using System;
+
+namespace GuidGetter
+{
+    public class GuidGetter
+    {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public string Get()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
