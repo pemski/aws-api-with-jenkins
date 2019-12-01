7f7f867b4cd6d23421a25d067e212a1dd5d1cd8e
Przemyslaw S <przmk01@gmail.com>
2019-12-01 16:25:46 +0100

    Lambda resource renamed

--- infrastructure/lambda/lambda.tf
+++ infrastructure/lambda/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "guid_api" {
   function_name    = "${local.resource_prefix}_guid-api"
   filename         = var.package_filename
   role             = aws_iam_role.iam_for_lambda.arn
