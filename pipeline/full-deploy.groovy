f38c04143764ad120b0ee8a560ce221df5b256db
Przemyslaw S <przmk01@gmail.com>
2019-12-14 18:12:11 +0100

    Base directory variable removed

--- pipeline/full-deploy.groovy
+++ pipeline/full-deploy.groovy
@@ -1,25 +1,14 @@
 pipeline {
     agent any
-    environment {
-        baseDir = """${powershell(
-            returnStdout: true,
-            script: '(Get-Item -Path ".\").FullName'
-        ).trim()}"""
-    }
     
     stages {
-        stage('Base directory') {
-            steps {
-                powershell 'echo "Base directory: ${baseDir}"'
-            }
-        }
         stage('Deploy ALB') {
             environment {
                 planFile = 'alb-plan'
             }
             steps {
                 powershell 'echo "Deploying ALB..."'
-                powershell 'cd "${baseDir}/infrastructure/alb"'
+                powershell 'cd "./infrastructure/alb"'
                 powershell 'terraform init'
                 powershell 'terraform plan -var ingress_cidr=178.37.89.16/32 -out="${planFile}"'
                 powershell 'terraform apply "${planFile}"'
