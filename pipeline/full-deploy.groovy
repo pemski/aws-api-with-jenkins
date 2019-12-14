7730eb108913efa5f5a8b28d55672e850ce02a63
Przemyslaw S <przmk01@gmail.com>
2019-12-14 18:39:37 +0100

    Fixed quotes in jenkinsfile to parse variables properly

--- pipeline/full-deploy.groovy
+++ pipeline/full-deploy.groovy
@@ -10,8 +10,8 @@ pipeline {
                 dir('./infrastructure/alb') {
                     powershell 'echo "Deploying ALB..."'
                     powershell 'terraform init'
-                    powershell 'terraform plan -var ingress_cidr=178.37.89.16/32 -out="${planFile}"'
-                    powershell 'terraform apply "${planFile}"'
+                    powershell "terraform plan -var ingress_cidr=178.37.89.16/32 -out=${planFile}"
+                    powershell "terraform apply ${planFile}"
                     powershell 'echo "ALB deployed."'
                 }
             }
@@ -24,8 +24,8 @@ pipeline {
                 dir('./infrastructure/lambda'){
                     powershell 'echo "Deploying Lambda api..."'
                     powershell 'terraform init'
-                    powershell 'terraform plan -out="${planFile}"'
-                    powershell 'terraform apply "${planFile}"'
+                    powershell "terraform plan -out=${planFile}"
+                    powershell "terraform apply ${planFile}"
                     powershell 'echo "Lambda api deployed."'
                 }
             }
