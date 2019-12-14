934cc3ab4dd5d92968aa7f53225623317cdb7d9c
Przemyslaw S <przmk01@gmail.com>
2019-12-14 18:03:56 +0100

    Minor fixes in scripts of pipeline and additional logs

--- pipeline/full-deploy.groovy
+++ pipeline/full-deploy.groovy
@@ -1,16 +1,15 @@
 pipeline {
     agent any
-    
     environment {
         baseDir = """${powershell(
-            script: '(Get-Item -Path ".\").FullName'
+            script: '(Get-Item -Path ".").FullName'
         )}"""
     }
     
     stages {
         stage('Base directory') {
             steps {
-                powershell 'echo \"Base directory: "${baseDir}"\"'
+                powershell 'echo "Base directory: ${baseDir}"'
             }
         }
         stage('Deploy ALB') {
@@ -18,15 +17,19 @@ pipeline {
                 planFile = 'alb-plan'
             }
             steps {
-                powershell 'cd "${baseDir}"/../infrastructure/alb'
+                powershell 'echo "Deploying ALB..."'
+                powershell 'cd "${baseDir}/infrastructure/alb"'
                 powershell 'terraform init'
                 powershell 'terraform plan -var ingress_cidr=178.37.89.16/32 -out="${planFile}"'
                 powershell 'terraform apply "${planFile}"'
+                powershell 'echo "ALB deployed."'
             }
         }
         stage('Deploy Lambda Api') {
             steps {
-                powershell 'echo "Placeholder for Lambda deploy"'
+                powershell 'echo "Deploying Lambda api..."'
+
+                powrshell 'echo "Lambda api deployed."'
             }
         }
     }
