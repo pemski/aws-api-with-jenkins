cfa82d705577ba2c14bb328348e1c17eaaf6510e
Przemyslaw S <przmk01@gmail.com>
2019-12-14 19:17:55 +0100

    Added pipeline for destroying infrastructure

---
+++ pipeline/full-destroy.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Destroy Lambda Api') {
+            steps {
+                dir('./infrastructure/lambda'){
+                    powershell 'echo "Destroying Lambda api..."'
+                    powershell 'terraform init'
+                    powershell 'terraform destroy -auto-approve'
+                    powershell 'echo "Lambda api destroyed."'
+                }
+            }
+        }
+        stage('Destroy ALB') {
+            steps {
+                dir('./infrastructure/alb') {
+                    powershell 'echo "Destroying ALB..."'
+                    powershell 'terraform init'
+                    powershell 'terraform destroy -auto-approve'
+                    powershell 'echo "ALB destroyed."'
+                }
+            }
+        }
+    }
+}
