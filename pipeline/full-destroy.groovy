6f331ca911dc23abe199d5c4fa11825fcfb864cb
Przemyslaw S <przmk01@gmail.com>
2019-12-15 20:49:45 +0100

    Default cidr value provided for ALB destroy command

--- pipeline/full-destroy.groovy
+++ pipeline/full-destroy.groovy
@@ -17,7 +17,7 @@ pipeline {
                 dir('./infrastructure/alb') {
                     powershell 'echo "Destroying ALB..."'
                     powershell 'terraform init'
-                    powershell 'terraform destroy -auto-approve'
+                    powershell 'terraform destroy -var ingress_cidr=0 -auto-approve'
                     powershell 'echo "ALB destroyed."'
                 }
             }
