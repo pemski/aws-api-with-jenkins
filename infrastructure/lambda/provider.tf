d5b520389021a49cc065a406ebbdfd5b0c58a02f
Przemyslaw S <przmk01@gmail.com>
2019-12-15 20:19:00 +0100

    Added S3 backend for ALB and Lambda

--- infrastructure/lambda/provider.tf
+++ infrastructure/lambda/provider.tf
@@ -3,3 +3,11 @@ provider "aws" {
   region  = "eu-west-1"
   version = "~> 2.0"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "psp-tf-state"
+    key    = "aws-guid-api/lambda"
+    region = "eu-west-1"
+  }
+}
