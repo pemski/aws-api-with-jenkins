7f96cedfc8d33be0ba4b8d7bfe58ed2f81ea56b2
Przemyslaw S <przmk01@gmail.com>
2019-12-15 23:18:11 +0100

    Default BRANCH value for deployment pipeline changed to 'master'

--- pipeline/seed_full_deploy.groovy
+++ pipeline/seed_full_deploy.groovy
@@ -2,7 +2,7 @@ pipelineJob('aws_guid_api/aws_guid_api_deployment') {
     definition {
         description('Deploys the guid api resources in AWS.')
         parameters {
-            stringParam('BRANCH', 'jenkinsfile', 'Name of the repository branch to run sources from.')
+            stringParam('BRANCH', 'master', 'Name of the repository branch to run sources from.')
             stringParam('ingress_cidr', '', 'CIDR value to be provided to allowed incoming traffic to ALB security group')
         }
         cpsScm {
